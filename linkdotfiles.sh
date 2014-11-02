ef8c631508be7fbbe4c6501f413328f05a638c02
Maximilian Irro <max@disposia.org>
2014-11-02 11:08:05 +0100

    updated output

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -12,8 +12,10 @@ REPOPATH=$(pwd)
 
 for DOT_F in "${DOT_LIST[@]}" ; do	
 	echo "removing ~/.$DOT_F" 
-	rm -r ~/.$DOT_F
+	rm -r ~/.$DOT_F >/dev/null 2>&1
 
 	echo "linking -$REPOPATH/$DOT_F"
 	ln -s $REPOPATH/$DOT_F ~/.$DOT_F
 done
+
+echo "all done. go and have fun with you setup!"

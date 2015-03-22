b998464be9173e48612e7348ae6988759fd64fb5
Maximilian Irro <max@disposia.org>
2015-03-22 16:29:11 +0100

    adapted new linking script (maybe I'll make a makefile one day - or something better)

--- linkdotfiles.sh
+++ linkdotfiles.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+REPOPATH=$(pwd)
+
+# list of binaries shipped in this repo
+BIN_LIST=(
+tree
+)
+
+# list of dotfiles shipped in this repo
 DOT_LIST=(
 vim
 bash_profile
@@ -9,14 +17,22 @@ vimrc
 ssh
 )
 
-REPOPATH=$(pwd)
+echo "setting up binaries"
+for BIN_F in "${BIN_LIST[@]}" ; do
+	cp $REPOPATH/bin/$BIN_F $HOME/bin/$BIN_F
+done
 
-for DOT_F in "${DOT_LIST[@]}" ; do	
-	echo "removing ~/.$DOT_F" 
+
+for DOT_F in "${DOT_LIST[@]}" ; do
+	echo "removing ~/.$DOT_F"
 	rm -r ~/.$DOT_F >/dev/null 2>&1
 
 	echo "linking -$REPOPATH/$DOT_F"
 	ln -s $REPOPATH/$DOT_F ~/.$DOT_F
 done
 
-echo "all done. go and have fun with you setup!"
+echo "sourcing profiles"
+source $HOME/.bash_profile
+source $HOME/.bashrc
+
+echo "all done. go and have fun!"

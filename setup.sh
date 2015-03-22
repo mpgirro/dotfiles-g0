6816923c549506d8b89c8135e27fae16562cf3d5
Maximilian Irro <max@disposia.org>
2015-03-22 16:40:55 +0100

    it seems echo does not understand \t the way the rest of the world does

--- setup.sh
+++ setup.sh
@@ -19,13 +19,13 @@ ssh
 
 echo "setting up binaries"
 for BIN_F in "${BIN_LIST[@]}" ; do
-	echo "\t=> $BIN_F"
+	echo "  ==> $BIN_F"
 	cp $REPOPATH/bin/$BIN_F $HOME/bin/$BIN_F
 done
 
 echo "linking dotfiles"
 for DOT_F in "${DOT_LIST[@]}" ; do
-	echo "\t=> $REPOPATH/$DOT_F"
+	echo "  ==> $REPOPATH/$DOT_F"
 	rm -r ~/.$DOT_F >/dev/null 2>&1
 	ln -s $REPOPATH/$DOT_F ~/.$DOT_F
 done
